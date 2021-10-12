3d45b100c36940185b79941753230759cbbd963b
Golden Ogbeka <ogbekagolden@gmail.com>
2021-10-12 14:08:05 +0100

    Tailwind and Mongoose Setup

---
+++ db/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('Database Connected');
+	} catch (error) {
+		console.log("Couldn't connect to Database");
+		// console.error(error);
+	}
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import express from "express";
+import express from 'express';
+import path from 'path';
+import db from './db/index.js';
+
+// To use __dirname and __filename when node app is in module mode
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Database Initialization
+db();
 
 const DEFAULT_SERVER_PORT = 4000;
-const app = express();
+const server = express();
 
-app.use(express.json());
+server.use(express.json());
 
-app.listen(process.env.SERVER_PORT || DEFAULT_SERVER_PORT, () => {
-  console.log(
-    `Server listening on port: ${
-      process.env.SERVER_PORT || DEFAULT_SERVER_PORT
-    }`
-  );
+// Added after all routes because of React's routing
+server.use(express.static(path.join(__dirname, 'client', 'build')));
+
+server.get('/*', async function (req, res) {
+	res.sendFile(path.join(__dirname, 'client', '/build/index.html'));
+});
+
+server.listen(process.env.PORT || DEFAULT_SERVER_PORT, () => {
+	console.log(
+		`Server listening on port: ${process.env.PORT || DEFAULT_SERVER_PORT}`,
+	);
 });
